b6fa4ccd223bc2b840477c144a9be641cd39fc4c
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-27 10:38:09 +0900

    feat: store todo

--- src/js/store/TodoStore.js
+++ src/js/store/TodoStore.js
@@ -1,1 +1,26 @@
-/* This file will be filled with on the lesson */
+import { EventEmitter } from "events";
+
+class TodoStore extends EventEmitter {
+  constructor() {
+    super();
+    this.todos = [
+      {
+        id: 891740174,
+        text: "Eat breakfirst",
+        complete: false
+      },
+      {
+        id: 9976342891,
+        text: "studing in the morning"
+      }
+    ];
+  }
+
+  getAll() {
+    return this.todos;
+  }
+}
+
+const todoStore = new TodoStore;
+
+export default todoStore;
